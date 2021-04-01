cc39aa7377ac1c24709a998224a3d63a0abaf782
ankitpithalia048 <62688379+ankitpithalia048@users.noreply.github.com>
2021-04-01 10:05:40 +0530

    added student table

--- Student.sql
+++ Student.sql
@@ -1,4 +1,9 @@
-create table Classes(
-	class_sec varchar(10),
-	stream varchar(10)
+create table Student (
+	id int primary key,
+	fname varchar(10),
+	lname varchar(10),
+	class_section varchar(10)
 );
+
+insert into Student values(100,'Ankit','Pithalias','10-A');
+select * from Student;
