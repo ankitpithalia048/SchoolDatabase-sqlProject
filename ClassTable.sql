bf60f8e690f6c920f3e2883cf75af97e3c73ae3f
ankitpithalia048 <62688379+ankitpithalia048@users.noreply.github.com>
2021-04-01 10:06:10 +0530

    added class table

--- ClassTable.sql
+++ ClassTable.sql
@@ -2,3 +2,9 @@ create table Classes(
 	class_sec varchar(10),
 	stream varchar(10)
 );
+
+
+insert into Classes values ('10-A','Maths');
+insert into Classes values ('10-B','PCB');
+insert into Classes values ('9-A','Commerce');
+select * from Classes;
