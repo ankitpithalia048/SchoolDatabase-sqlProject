076b67b859c01eaf2226bb3baf5f63dfb1e85ea0
ankitpithalia048 <62688379+ankitpithalia048@users.noreply.github.com>
2021-04-01 10:05:06 +0530

    added subject table

--- SubjectTable.sql
+++ SubjectTable.sql
@@ -1,5 +1,10 @@
-  
-create table Subjects(
-	subj_id varchar(10),
-	subj_name varchar(10)
+create table Subject(
+	subject_id varchar(10),
+	subject_name varchar(10)
 );
+
+  
+insert into Subject values ('001','Maths');
+insert into Subject values ('002','Physics');
+
+select * from Subject;
